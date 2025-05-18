9dda97a6fa68d5d37fb902e3a46ae3e2d8a030a5
RIFKY <rifkip1337@gmail.com>
2025-05-18 16:13:27 +0800

    build(tools): (#10) theme switcher

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class', // or 'media' or 'class'
+  content: [
+    "./*.html",
+    "./projects/**/*.html",
+    "./src/**/*.js", // if you have js files that manipulate classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
